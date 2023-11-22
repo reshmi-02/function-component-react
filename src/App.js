021ee819e599d5f3419030326f21b06d4815969c
reshmi-02 <reshmihash123@gmail.com>
2023-11-22 13:31:35 +0530

    Addnew

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Function from "./component/function.js";
+function App() {
+  return (
+    <div>
+      <Function/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/function.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Function =()=>{
+    return(
+        <div>
+            Function Component
+        </div>
+    )
+}
+
+export default Function
